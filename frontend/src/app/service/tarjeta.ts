5391c538f65b820100d8c7e13f1e8b4e09eb8952
tatianap69 <angieepaez69@gmail.com>
2025-08-06 09:53:47 -0500

    Añadir observable

--- frontend/src/app/service/tarjeta.ts
+++ frontend/src/app/service/tarjeta.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Tarjeta {
-  
+  private myApiUrl = `/api/Tarjeta/`;
+
+  constructor(private http: HttpClient) {}
+  getListadoTarjetas(): Observable<any> {
+    return this.http.get(this.myApiUrl);
+  }
 }
