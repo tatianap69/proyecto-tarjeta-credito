1d87b609e290f80d798e4293000cefece7f83c4b
tatianap69 <angieepaez69@gmail.com>
2025-08-06 09:51:53 -0500

    Mostrar componente tarjeta de crédito

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TarjetaCredito } from './components/tarjeta-credito/tarjeta-credito';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TarjetaCredito, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
